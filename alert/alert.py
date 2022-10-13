f9fc4baa6c6d6676ea3fde805b61cd538e8ade95
John Lukach <84751182+jblukach@users.noreply.github.com>
2022-10-13 01:30:54 +0000

    organization

--- alert/alert.py
+++ alert/alert.py
@@ -7,41 +7,11 @@ def handler(event, context):
     account = os.environ['ACCOUNT']
     region = os.environ['REGION']
 
+    awsaccount = 'arn:aws:access-analyzer:'+region+':'+account+':analyzer/awsaccount'
+    organization = 'arn:aws:access-analyzer:'+region+':'+account+':analyzer/organization'
+
     accessanalyzer = boto3.client('accessanalyzer')
     sns_client = boto3.client('sns')
-
-    awsaccount = 'arn:aws:access-analyzer:'+region+':'+account+':analyzer/awsaccount'
-    
-    paginator = accessanalyzer.get_paginator('list_findings')
-    
-    pages = paginator.paginate(
-        analyzerArn = awsaccount
-    )
-    
-    for page in pages:
-        for finding in page['findings']:
-            try:
-                if finding['error'] == 'ACCESS_DENIED' and finding['status'] != 'ARCHIVED':
-                    print(finding)
-                    response = sns_client.publish(
-                        TopicArn = os.environ['SNS_TOPIC'],
-                        Subject = 'IAM Access Analyzer Alert',
-                        Message = str(finding)
-                    )
-            except:
-                pass
-            try:
-                if finding['isPublic'] == True and finding['status'] != 'ARCHIVED':
-                    print(finding)
-                    response = sns_client.publish(
-                        TopicArn = os.environ['SNS_TOPIC'],
-                        Subject = 'IAM Access Analyzer Alert',
-                        Message = str(finding)
-                    )
-            except:
-                pass
-
-    organization = 'arn:aws:access-analyzer:'+region+':'+account+':analyzer/organization'
 
     paginator = accessanalyzer.get_paginator('list_findings')
     
